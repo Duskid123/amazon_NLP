0e6eec9d63e2c8e4eeb7bc2cb6647bda29722b38
Duskid123 <duskisdovid@gmail.com>
2024-11-21 15:41:13 -0500

    Made it so that cleaning the code should work.

--- .venv/Clean data.py
+++ .venv/Clean data.py
@@ -3,7 +3,8 @@ import re  # For regular expressions
 from nltk.tokenize import word_tokenize  # To split text into words
 from nltk.stem import WordNetLemmatizer  # To lemmatize words
 from nltk.corpus import stopwords
-import time
+import time, os, subprocess
+
 
 # Initialize the lemmatizer
 lemmatizer = WordNetLemmatizer()
@@ -32,7 +33,20 @@ try:
     start_time = time.time()
 
     # if you wanted to clean all of the data just don't use sample
-    data = pd.read_csv('./DataSet/unCleanedData/rejoined_fileUncleaned.csv')
+    file_to_check = './DataSet/unCleanedData/rejoined_fileUncleaned.csv'
+    script_to_run = "./DataSet/unCleanedData/RejoinCSV.py"
+
+    if not os.path.exists(file_to_check):
+        try:
+            subprocess.run(["python", script_to_run], check=True)
+            print(f"Successfully ran {script_to_run}.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running {script_to_run}: {e}")
+    else:
+        print(f"{file_to_check} already exists. No action taken.")
+    data = pd.read_csv(file_to_check, header=0)
+    data = data.drop(data.columns[0], axis=1)
+    # data = pd.read_csv('./DataSet/unCleanedData/rejoined_fileUncleaned.csv')
 except ValueError:
     print("Error loading JSON file. Please check the file path and format.")
 else:
